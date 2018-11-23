eab8942049ad3b1a5dbcf3c21791d618fa292029
krzysztofhejna <khejna15@wp.pl>
2018-11-23 23:12:32 +0100

    Add tidbits

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
-var output = document.getElementById('converter__output');
 var button = document.getElementById('converter__button');
+var output = document.getElementById('converter__output');
+var tidbit = document.getElementById('tidbit');
 var temperatureC;
 var temperatureF;
 
@@ -9,9 +10,16 @@ button.addEventListener('click', function(){
     temperatureC = window.prompt('What is the temperature in Celcius?');
     if (temperatureC && !isNaN(temperatureC) && temperatureC > -273.15) {
         temperatureF = 1.8 * temperatureC + 32;
-        output.innerHTML = (temperatureC + ' degrees Celcius is ' + temperatureF + ' degrees Fahrenheit.'+'<br><br') + output.innerHTML;
+        output.innerHTML =(temperatureC + ' degrees Celcius is ' + temperatureF + ' degrees Fahrenheit.<br><br');
+        if (temperatureC < 0) {
+            tidbit.innerHTML = ('At ' + temperatureC + ' degrees Celcius water is in a solid state.<br><br>');
+        } else if (temperatureC >= 0 && temperatureC <100) {
+            tidbit.innerHTML = ('At ' + temperatureC + ' degrees Celcius water is in a liquid state.<br><br>');
+        } else {
+            tidbit.innerHTML = ('At ' + temperatureC + ' degrees Celcius water is in a vapor state.<br><br>');
+        }
     }
     else {
         alert('Please enter a valid temperature in Celcius');
-    }
+    };
 });

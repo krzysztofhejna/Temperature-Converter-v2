64c579aa9958fd3c00c8a81ddbe834cb0c6e343a
krzysztofhejna <khejna15@wp.pl>
2018-11-25 12:29:11 +0100

    Update javascript code with functions

--- js/script.js
+++ js/script.js
@@ -1,58 +1,70 @@
 'use strict';
-var button1 = document.getElementById('converterCtoF-btn');
-var converterCtoF = document.getElementById('converterCtoF')
-var button2 = document.getElementById('converterFtoC-btn');
-var converterFtoC = document.getElementById('converterFtoC')
-var temperatureC;
-var temperatureF;
+(function() {
+    var button1 = document.getElementById('converterCtoF-btn');
+    var converterCtoF = document.getElementById('converterCtoF');
+    var button2 = document.getElementById('converterFtoC-btn');
+    var converterFtoC = document.getElementById('converterFtoC');
+    var temperatureC;
+    var temperatureF;
 
-button1.addEventListener('click', function(){
-    var minC = -273.15 // absolute zero temperature in Celsius
-    var output1 = converterCtoF.querySelector('#converterCtoF-output');
-    var tidbit1 = converterCtoF.querySelector('#converterCtoF-tidbit');
-    temperatureC = window.prompt('What is the temperature in Celsius?');
+    // This function determines if the entered input is valid
+    var isInputValid = function(temperature, min) {
+        if (!temperature || isNaN(temperature) || temperature <= min) {
+            return alert('Please enter a valid temperature');;
+        }
 
-    if (!temperatureC || isNaN(temperatureC) || temperatureC <= minC) {
-        return alert('Please enter a valid temperature in Celsius');;
+        return true;
     }
 
-    temperatureF = 1.8 * temperatureC + 32; // formula to convert temperature in Celsius to Fahrenheit
-    output1.innerHTML = (temperatureC + ' degrees Celsius is ' + temperatureF + ' degrees Fahrenheit.<br><br');
-
-    if (temperatureC < 0) {
-        return tidbit1.innerHTML = ('At ' + temperatureC + ' degrees Celcius water is in a solid state.<br><br>');
+    // This function converts from Celsius to Fahrenheit
+    var convertCtoF = function (temperatureC) {
+        temperatureF = 1.8 * temperatureC + 32; // formula to convert temperature in Celsius to Fahrenheit
+        temperatureF = +temperatureF.toFixed (2); // limiting the output temperature to at most 2 decimal points
+        var output1 = converterCtoF.querySelector('#converterCtoF-output');
+        output1.innerHTML = (temperatureC + ' degrees Celsius is ' + temperatureF + ' degrees Fahrenheit.<br><br');
+        return temperatureF;
     }
 
-    if (temperatureC >= 0 && temperatureC < 100) {
-        return tidbit1.innerHTML = ('At ' + temperatureC + ' degrees Celcius water is in a liquid state.<br><br>');
+    // This function converts from Fahrenheit to Celsius
+    var convertFtoC = function (temperatureF) {
+        temperatureC = (temperatureF - 32) * (5/9); // formula to convert temperature in Fahrenheit to Celsius
+        temperatureC = +temperatureC.toFixed(2); // limiting the output temperature to at most 2 decimal points
+        var output2 = converterFtoC.querySelector('#converterFtoC-output');
+        output2.innerHTML =(temperatureF + ' degrees Fahrenheit is ' + temperatureC + ' degrees Celsius.<br><br');
+        return temperatureC;
     }
 
-    return tidbit1.innerHTML = ('At ' + temperatureC + ' degrees Celcius water is in a vapor state.<br><br>');
-});
-
-
-button2.addEventListener('click', function(){
-    var minF = -459.67; // absolute zero temperature in Fahrenheit
-    var tempF1 = 32; // temperature of 0 degrees Celsius in Fahrenheit
-    var tempF2 = 212 // temperature of 100 degrees Celsius in Fahrenheit
-    var output2 = converterFtoC.querySelector('#converterFtoC-output');
-    var tidbit2 = converterFtoC.querySelector('#converterFtoC-tidbit');
-    temperatureF = window.prompt('What is the temperature in Fahrenheit');
-
-    if (!temperatureF || isNaN(temperatureF) || temperatureF <= minF) {
-        return alert('Please enter a valid temperature in Fahrenheit');;
+    // This function will provide an information on the state of water at input temperature
+    var waterState = function (temperatureC, tidbit) {
+        if (temperatureC < 0) {
+            return tidbit.innerHTML = ('At this temperature water is in a solid state.<br><br>');
+        }
+    
+        if (temperatureC >= 0 && temperatureC < 100) {
+            return tidbit.innerHTML = ('At this temperature water is in a liquid state.<br><br>');
+        }
+    
+        return tidbit.innerHTML = ('At this temperature water is in a vapor state.<br><br>');
     }
 
-    temperatureC = (temperatureF - 32) * (5/9); // formula to convert temperature in Fahrenheit to Celsius
-    output2.innerHTML =(temperatureF + ' degrees Fahrenheit is ' + temperatureC + ' degrees Celsius.<br><br');
 
-    if (temperatureC < tempF1) {
-        return tidbit2.innerHTML = ('At ' + temperatureF + ' degrees Fahrenheit water is in a solid state.<br><br>');
-    }
+    button1.addEventListener('click', function(){
+        var minC = -273.15 // absolute zero temperature in Celsius
+        var tidbit1 = converterCtoF.querySelector('#converterCtoF-tidbit');
+        temperatureC = window.prompt('What is the temperature in Celsius?');
+        if (isInputValid(temperatureC, minC)) {
+            convertCtoF(temperatureC);
+            waterState(temperatureC, tidbit1);
+        }
+    });
 
-    if (temperatureC >= tempF1 && temperatureC < tempF2) {
-        return tidbit2.innerHTML = ('At ' + temperatureF + ' degrees Fahrenheit water is in a liquid state.<br><br>');
-    }
-
-    return tidbit2.innerHTML = ('At ' + temperatureF + ' degrees Fahrenheit water is in a vapor state.<br><br>');
-});
+    button2.addEventListener('click', function(){
+        var minF = -459.67; // absolute zero temperature in Fahrenheit
+        var tidbit2 = converterFtoC.querySelector('#converterFtoC-tidbit');
+        temperatureF = window.prompt('What is the temperature in Fahrenheit');
+        if (isInputValid(temperatureF, minF)) {
+            convertFtoC(temperatureF);
+            waterState(temperatureC, tidbit2);
+        }
+    });
+})();
